090fdceb3bd8dad1252d67c0f3f2986cfad7327c
AndreyTarkaniy <endrubass@gmail.com>
2023-03-13 19:22:25 +0200

    add js-code

---
+++ src/js/03-feedback.js
@@ -0,0 +1,39 @@
+import   _throttle  from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let formData ={};
+
+form.addEventListener('submit', FormSubmit);
+form.addEventListener('input', _throttle(onFormInput, 500));
+
+formLoad();
+
+function FormSubmit(event) {
+  event.preventDefault();
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log("Отправили",savedData);
+  
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+function onFormInput(event) {
+  formData[event.target.name]=event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+}
+
+function formLoad() {
+  const formLoad = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!formLoad) { 
+    return ;
+  };
+  form.email.value = formLoad.email || '';
+  form.message.value = formLoad.message || '';
+  formData.email =form.email.value ;
+  formData.message =form.message.value ;   
+
+}
